fa5c69def9061cdfcb80b3e1bbe9c65ec04196c5
Carlo Cabanilla <carlo@datadoghq.com>
2011-12-02 11:24:53 -0500

    get config from ~/.dogrc or specified config file

--- lib/eventer.rb
+++ lib/eventer.rb
@@ -35,18 +35,57 @@ end
 def eventer(start_time=nil, api_key=nil, application_key=nil, &block)
   require 'optparse'
   
-  api_key = nil
-  app_key = nil
-  start_time = nil
-
+  all_config = {
+    :default => {:start_time => Time.now.to_i - 60*60},
+    :cli => {},
+    :file => {}
+  }
+  config_file = "~/.dogrc"
+  
   optparse = OptionParser.new do |opts|
-    opts.banner = "Usage: eventer [--app_key=APP_KEY] --api_key=API_KEY --time=TIME"
-    opts.on("-t", "--time TIME", Float, "start time") { |v| start_time = v }
-    opts.on("--api_key API_KEY", "api key") { |v| api_key = v }
-    opts.on("--app_key [APP_KEY]", "app key") { |v| app_key = v }
+    opts.banner = "Usage: ruby #{$0} [options]"
+    opts.on("-t", "--time TIME", Float, "Start time in seconds since unix epoch. Defaults to the current time minus an hour.") { |v| 
+      all_config[:cli][:start_time] = start_time
+    }
+    opts.on("-c", "--config CONFIG_FILE", "Path to config file. Defaults to #{config_file}") { |v|
+      config_file = v
+    }
+    opts.on("--api_key API_KEY", "Api key. Overrides config values.") { |v| 
+      all_config[:cli][:api_key] = v
+    }
+    opts.on("--app_key [APP_KEY]", "App key. Overrides config values.") { |v| 
+      all_config[:cli][:app_key] = v
+    }
+    
   end
   optparse.parse!
+
+  if not config_file
+    config_file = default_config_file
+  end
   
-  Eventer::Eventer.new(api_key, application_key).generate start_time, &block
+  config_file = File.expand_path config_file
+  if File.exists? config_file
+    File.open(config_file) do |f| 
+      f.readlines.each do |line|
+        keyval = line.split "="
+        if keyval.length == 2
+          key = keyval[0].strip
+          val = keyval[1].strip
+          if key == 'apikey'
+            all_config[:file][:api_key] = val
+          elsif key == 'appkey'
+            all_config[:file][:app_key] = val
+          end
+        end
+      end
+    end
+  end
+  
+  config = all_config.reduce({}) do |merged, to_merge|
+    merged.merge to_merge[1]
+  end
+  
+  Eventer::Eventer.new(config[:api_key], config[:app_key]).generate config[:start_time], &block
 end
 
